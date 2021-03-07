8bff540023b2628ae75095cff5f50542257912f2
Oumaima El youssefi <oel-yous@e2r2p12.1337.ma>
2021-03-07 17:22:00 +0100

    mysql and grafana done

---
+++ srcs/grafana/srcs/start.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash
+/grafana-7.3.2/bin/grafana-server --homepath grafana-7.3.2
+/bin/sh
---
+++ srcs/mysql/srcs/start.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+rc-status
+touch /run/openrc/softlevel
+/etc/init.d/mariadb setup
+rc-service mariadb start
+echo "CREATE USER 'admin'@'localhost'  IDENTIFIED BY '123';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON admin.* TO 'admin'@'localhost' IDENTIFIED BY '123';" | mysql -u root
+
+/bin/sh
